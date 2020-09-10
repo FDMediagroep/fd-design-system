37a22476285e62d95644fc11be1ee106cb8aa784
Willem Liu <willem.liu@fdmediagroep.nl>
2020-09-10 12:55:47 +0200

    Redesign buttons

--- src/pages/button/default.tsx
+++ src/pages/button/default.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Explain } from '../../components/Explain';
 import { Button, getCssClassNames } from '../../components/button/Button';
 import Head from 'next/head';
+import { EnvelopeIcon } from '../../design-tokens/icons';
+import styles from './buttons.module.scss';
+
 const metaTitle = 'Button';
 const metaDescription =
     'Button, used to interact with the content: comment, share and vote';
@@ -24,6 +27,7 @@ function DefaultButton() {
             </Head>
 
             <Explain
+                previewClassName={styles.buttons}
                 anchor="default"
                 cssClassNames={getCssClassNames()}
                 legend="Button (default)"
@@ -41,7 +45,22 @@ function DefaultButton() {
                     </>
                 }
             >
-                <Button id="demoButtonS">Button (small)</Button>
+                <>
+                    <Button id="demoButtonS">Button (small)</Button>
+                    <Button id="demoButtonSIcon">
+                        <span
+                            dangerouslySetInnerHTML={{ __html: EnvelopeIcon }}
+                        />
+                        <span>Icon (small)</span>
+                    </Button>
+
+                    <Button id="demoButtonDisabled" disabled="disabled">
+                        <span
+                            dangerouslySetInnerHTML={{ __html: EnvelopeIcon }}
+                        />
+                        <span>Disabled (small)</span>
+                    </Button>
+                </>
             </Explain>
 
             <Explain
@@ -64,29 +83,6 @@ function DefaultButton() {
             >
                 <Button id="demoButtonM" size="m">
                     Button (medium)
-                </Button>
-            </Explain>
-
-            <Explain
-                anchor="l"
-                cssClassNames={getCssClassNames()}
-                legend="Button (L)"
-                reactComponentName="Button"
-                description={
-                    <>
-                        <p>
-                            Used to interact with the content: comment, share
-                            and vote.
-                        </p>
-                        <p>
-                            Supportive: button supports editorial content, but
-                            is not the most important item on the page.
-                        </p>
-                    </>
-                }
-            >
-                <Button id="demoButtonL" size="l">
-                    Button (large)
                 </Button>
             </Explain>
         </>
