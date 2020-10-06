0f1d46e6137a5597908092f0adec818978c93a21
Willem Liu <willem.liu@fdmediagroep.nl>
2020-10-06 15:46:28 +0200

    More menu button

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,6 +3,7 @@ import {
     ChevronDownThinIcon,
     ChevronUpThinIcon,
 } from '../../design-tokens/icons';
+import { debounce } from '../../utils/debounce';
 import styles from './Menu.module.scss';
 
 declare let ResizeObserver: any;
@@ -44,6 +45,11 @@ interface Props {
      * Menu items. Use a nested structure to create sub-menus.
      */
     menuItems?: MenuItem[];
+    /**
+     * Label of the `More` menu-item.
+     * Default: Meer
+     */
+    moreLabel?: string;
     [x: string]: any;
 }
 
@@ -55,6 +61,9 @@ interface Props {
 function generateIds(menuItems: MenuItem[], parent?: string) {
     const menuItemsCopy = [...menuItems];
     menuItemsCopy.forEach((menuItem, idx) => {
+        if (menuItem.id) {
+            return true;
+        }
         menuItem.id = `${
             parent ? `${parent}-${idx + 1}` : `menu-item-${idx + 1}`
         }`;
@@ -65,70 +74,122 @@ function generateIds(menuItems: MenuItem[], parent?: string) {
     return menuItemsCopy;
 }
 
-/**
- * Given ltr order. rect1 should come before rect2.
- * We simply check if rect2 is passed over rect1.
- *
- * @param rect1
- * @param rect2
- */
-function overlap(rect1: DOMRect, rect2: DOMRect) {
-    return rect1.right > rect2.left;
-}
+let expandTimeout;
 
 function Menu(props: Props) {
     const menuRef = useRef(null);
     const customMenuRef = useRef(null);
 
+    const [moreMenuItem, setMoreMenuItem] = useState<MenuItem>({
+        id: styles['more-menu'],
+        label: props.moreLabel ?? 'Meer',
+        link: '',
+        menuItems: [{ id: 'test', label: 'test', link: '/test' }],
+    });
     const [menuItems, setMenuItems] = useState<MenuItem[]>(
         generateIds(props.menuItems)
     );
+    const [sortedMenuItems, setSortedMenuItems] = useState<MenuItem[]>(
+        menuItems
+    );
 
     useEffect(() => {
-        setMenuItems(generateIds(props.menuItems));
-    }, [props.menuItems]);
+        const renderMenuItems = generateIds(
+            moreMenuItem.menuItems.length
+                ? [...props.menuItems, moreMenuItem]
+                : props.menuItems
+        );
+        setMenuItems(renderMenuItems);
+        setSortedMenuItems(renderMenuItems);
+    }, [props.menuItems, moreMenuItem]);
+
+    /**
+     * Check overlap and re-order menu-items.
+     */
+    const handleOverlap = useCallback(() => {
+        if (menuRef.current && customMenuRef.current) {
+            let newMenuItems: MenuItem[] = [];
+            let overlappedItems: MenuItem[] = [];
+            let overlappedMoreMenuItems: MenuItem[] = [];
+            let newMoreMenuItem = JSON.parse(JSON.stringify(moreMenuItem));
+            const availableWidth =
+                menuRef.current.getBoundingClientRect().width -
+                customMenuRef.current.getBoundingClientRect().width;
+            const moreMenu = menuRef.current.querySelector(
+                `#${newMoreMenuItem.id}`
+            );
+            let accumulativeWidth = moreMenu?.getBoundingClientRect()?.width;
+
+            menuItems.forEach((menuItem) => {
+                if (menuItem.id !== styles['more-menu']) {
+                    const newMenuItem = JSON.parse(JSON.stringify(menuItem));
+                    const cur = menuRef.current.querySelector(
+                        `#${menuItem.id}`
+                    );
+                    if (
+                        cur &&
+                        accumulativeWidth + cur?.getBoundingClientRect().width <
+                            availableWidth
+                    ) {
+                        newMenuItems.push(newMenuItem);
+                        /**
+                         * Filter the newMenuItem from the more menu sub-menu.
+                         */
+                        newMoreMenuItem.menuItems = [
+                            ...newMoreMenuItem.menuItems.filter(
+                                (menuItem) => menuItem.id !== newMenuItem.id
+                            ),
+                        ];
+                        accumulativeWidth += cur?.getBoundingClientRect().width;
+                    } else {
+                        overlappedItems.push(newMenuItem);
+                        overlappedMoreMenuItems.push({
+                            ...JSON.parse(JSON.stringify(newMenuItem)),
+                            id: `more-${newMenuItem.id}`,
+                        });
+                    }
+                }
+            });
+
+            newMoreMenuItem.menuItems.unshift(...overlappedMoreMenuItems);
+
+            setSortedMenuItems([
+                ...newMenuItems,
+                newMoreMenuItem,
+                ...overlappedItems,
+            ]);
+        }
+    }, [menuRef.current, customMenuRef.current, menuItems, moreMenuItem]);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => debounce(handleOverlap, 50));
+        handleOverlap(); // Initial check
+    }, [handleOverlap]);
 
     /**
      * Determine which menu items don't fit anymore and hide them.
      * If there is enough space to show them again they will be displayed again.
      */
     useEffect(() => {
-        if (menuRef.current && customMenuRef.current) {
-            [].slice
-                .call(menuRef.current.querySelectorAll('nav > ul > li'))
-                .forEach((li: HTMLElement) => {
-                    const check = () => {
-                        if (
-                            overlap(
-                                li.getBoundingClientRect(),
-                                customMenuRef.current.getBoundingClientRect()
-                            )
-                        ) {
-                            requestAnimationFrame(() => {
-                                li.classList.add('visuallyhidden');
-                            });
-                        } else {
-                            li.classList.remove('visuallyhidden');
-                        }
-                    };
-
-                    window.addEventListener('resize', check);
-                    check(); // Initial check
-                });
-
-            const observer = new ResizeObserver((mutations) => {
+        if (customMenuRef.current) {
+            const observer = new ResizeObserver(() => {
                 window.dispatchEvent(new CustomEvent('resize'));
             });
             observer.observe(customMenuRef.current);
         }
-    }, [menuRef.current, customMenuRef.current]);
+    }, [customMenuRef.current]);
 
     function toggle(id: string, subMenuItems?: MenuItem[]) {
-        if (!menuItems.length) {
+        if (!sortedMenuItems.length) {
             return;
         }
+        if (expandTimeout) {
+            clearTimeout(expandTimeout);
+        }
         const menuItemsCopy =
-            subMenuItems && subMenuItems.length ? subMenuItems : [...menuItems];
+            subMenuItems && subMenuItems.length
+                ? subMenuItems
+                : [...sortedMenuItems];
         menuItemsCopy.forEach((menuItem) => {
             if (id === menuItem.id) {
                 menuItem.expanded = !menuItem.expanded;
@@ -138,150 +199,142 @@ function Menu(props: Props) {
         });
         if (!subMenuItems.length) {
             // only call for the root
-            setMenuItems(menuItemsCopy);
+            setSortedMenuItems(menuItemsCopy);
         }
     }
 
-    const expand = useCallback(
-        (id: string, subMenuItems?: MenuItem[]) => {
-            if (!menuItems.length) {
-                return;
+    const expand = (id: string, subMenuItems?: MenuItem[]) => {
+        if (!sortedMenuItems.length) {
+            return;
+        }
+        if (expandTimeout) {
+            clearTimeout(expandTimeout);
+        }
+        const menuItemsCopy =
+            subMenuItems && subMenuItems.length
+                ? subMenuItems
+                : [...sortedMenuItems];
+        menuItemsCopy.forEach((menuItem) => {
+            if (id === menuItem.id) {
+                menuItem.expanded = true;
+            } else if (menuItem?.menuItems?.length) {
+                expand(id, menuItem.menuItems);
             }
+        });
+        if (!subMenuItems.length) {
+            // only call for the root
+            setSortedMenuItems(menuItemsCopy);
+        }
+    };
+
+    const contract = (id?: string, subMenuItems?: MenuItem[]) => {
+        if (!sortedMenuItems.length) {
+            return;
+        }
+        expandTimeout = setTimeout(() => {
             const menuItemsCopy =
                 subMenuItems && subMenuItems.length
                     ? subMenuItems
-                    : [...menuItems];
+                    : [...sortedMenuItems];
             menuItemsCopy.forEach((menuItem) => {
-                if (id === menuItem.id) {
-                    menuItem.expanded = true;
-                } else if (menuItem?.menuItems?.length) {
-                    expand(id, menuItem.menuItems);
+                if (!id || id === menuItem.id) {
+                    menuItem.expanded = false;
+                    if (menuItem?.menuItems?.length) {
+                        contract(null, menuItem.menuItems);
+                    }
                 }
             });
             if (!subMenuItems.length) {
                 // only call for the root
-                setMenuItems(menuItemsCopy);
+                setSortedMenuItems(menuItemsCopy);
             }
-        },
-        [menuItems]
-    );
-
-    const contract = useCallback(
-        (id?: string, subMenuItems?: MenuItem[]) => {
-            if (!menuItems.length) {
-                return;
-            }
-            setTimeout(() => {
-                const menuItemsCopy =
-                    subMenuItems && subMenuItems.length
-                        ? subMenuItems
-                        : [...menuItems];
-                menuItemsCopy.forEach((menuItem) => {
-                    if (!id || id === menuItem.id) {
-                        menuItem.expanded = false;
-                        if (menuItem?.menuItems?.length) {
-                            contract(null, menuItem.menuItems);
-                        }
-                    }
-                });
-                if (!subMenuItems.length) {
-                    // only call for the root
-                    setMenuItems(menuItemsCopy);
-                }
-            }, 100);
-        },
-        [menuItems]
-    );
+        }, 100);
+    };
 
     /**
-     * Render the menu items.
+     * Render all the menu-items.
      * @param menuItems
+     * @param hidden
+     * @param isRoot true when the item is the root menu-item
      */
-    const renderMenu = useCallback(
-        (menuItems: MenuItem[], hidden?: boolean, isRoot = true) => {
-            const menuComponent = (
-                <ul>
-                    {menuItems.map((menuItem) => {
-                        const hasPopup =
-                            menuItem.menuItems && menuItem.menuItems.length > 0;
+    const renderMenu = (
+        menuItems: MenuItem[],
+        hidden = false,
+        isRoot = true
+    ) => {
+        const items = menuItems.map((menuItem) => {
+            const hasPopup =
+                menuItem.menuItems && menuItem.menuItems.length > 0;
 
-                        return (
-                            <li
-                                id={menuItem.id}
-                                key={menuItem.id}
-                                className={styles.hasItems}
-                                onMouseEnter={
-                                    isRoot
-                                        ? expand.bind(null, menuItem.id)
-                                        : null
-                                }
-                                onMouseLeave={
-                                    isRoot
-                                        ? contract.bind(null, menuItem.id)
-                                        : null
-                                }
-                            >
-                                <a
-                                    href={menuItem.link}
-                                    title={menuItem.id}
-                                    {...(hasPopup
-                                        ? {
-                                              'aria-expanded': !!menuItem.expanded,
-                                          }
-                                        : {})}
-                                    aria-haspopup={hasPopup}
-                                >
-                                    {menuItem.label}
-                                </a>
-                                {hasPopup && (
-                                    <button
-                                        onClick={toggle.bind(null, menuItem.id)}
-                                        dangerouslySetInnerHTML={{
-                                            __html: menuItem.expanded
-                                                ? ChevronUpThinIcon
-                                                : ChevronDownThinIcon,
-                                        }}
-                                        aria-label={
-                                            menuItem.expanded
-                                                ? 'close sub-menu'
-                                                : 'open sub-menu'
-                                        }
-                                        aria-expanded={menuItem.expanded}
-                                        aria-haspopup={hasPopup}
-                                    />
-                                )}
-                                {menuItem.menuItems &&
-                                    menuItem.menuItems.length &&
-                                    renderMenu(
-                                        menuItem.menuItems,
-                                        !menuItem.expanded,
-                                        false
-                                    )}
-                            </li>
-                        );
-                    })}
-                </ul>
+            return (
+                <li
+                    id={menuItem.id}
+                    key={menuItem.id}
+                    className={styles.hasItems}
+                    onMouseLeave={
+                        isRoot ? contract.bind(null, menuItem.id) : null
+                    }
+                >
+                    <a
+                        href={menuItem.link}
+                        title={menuItem.id}
+                        {...(hasPopup
+                            ? {
+                                  'aria-expanded': !!menuItem.expanded,
+                              }
+                            : {})}
+                        aria-haspopup={hasPopup}
+                        onMouseEnter={
+                            isRoot ? expand.bind(null, menuItem.id) : null
+                        }
+                    >
+                        {menuItem.label}
+                    </a>
+                    {hasPopup && (
+                        <button
+                            onClick={toggle.bind(null, menuItem.id)}
+                            dangerouslySetInnerHTML={{
+                                __html: menuItem.expanded
+                                    ? ChevronUpThinIcon
+                                    : ChevronDownThinIcon,
+                            }}
+                            aria-label={
+                                menuItem.expanded
+                                    ? 'close sub-menu'
+                                    : 'open sub-menu'
+                            }
+                            aria-expanded={menuItem.expanded}
+                            aria-haspopup={hasPopup}
+                            onMouseLeave={
+                                isRoot ? contract.bind(null, menuItem.id) : null
+                            }
+                        />
+                    )}
+                    {menuItem.menuItems &&
+                        menuItem.menuItems.length > 0 &&
+                        renderMenu(
+                            menuItem.menuItems,
+                            !menuItem.expanded,
+                            false
+                        )}
+                </li>
             );
+        });
 
-            return isRoot ? (
-                menuComponent
-            ) : (
-                <div className={hidden ? 'hidden' : ''}>{menuComponent}</div>
-            );
-        },
-        [props.menuItems]
-    );
+        return <ul className={hidden ? 'hidden' : ''}>{items}</ul>;
+    };
 
     return (
         <header
+            ref={menuRef}
             className={`${styles.menuContainer}${
                 props.className ? ` ${props.className}` : ''
             }`}
             aria-label={props.label}
         >
             <div className={styles.menuCenter}>
-                <nav ref={menuRef} className={styles.menu}>
-                    {renderMenu(menuItems)}
+                <nav className={styles.menu}>
+                    {renderMenu(sortedMenuItems ?? menuItems)}
                 </nav>
                 <span
                     ref={customMenuRef}
