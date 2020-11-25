99427d1d1ae4dfdab24c923c108399222a089ad4
Willem Liu <willem.liu@fdmediagroep.nl>
2020-11-25 16:58:19 +0100

    body-text serif m is reserved

--- src/pages/typography.tsx
+++ src/pages/typography.tsx
@@ -140,7 +140,7 @@ function Page() {
                 <>
                     <p className="body-text serif xs">body-text serif xs</p>
                     <p className="body-text serif s">body-text serif s</p>
-                    <p className="body-text serif m">body-text serif m</p>
+                    <p className="body-text serif m">Reserved</p>
                     <p className="body-text serif l">Reserved</p>
                     <p className="body-text serif xl">Reserved</p>
                     <p className="body-text serif xxl">Reserved</p>
@@ -164,9 +164,7 @@ function Page() {
                     <p className="body-text serif s bold">
                         body-text serif s bold
                     </p>
-                    <p className="body-text serif m bold">
-                        body-text serif m bold
-                    </p>
+                    <p className="body-text serif m bold">Reserved</p>
                     <p className="body-text serif l bold">Reserved</p>
                     <p className="body-text serif xl bold">Reserved</p>
                     <p className="body-text serif xxl bold">Reserved</p>
