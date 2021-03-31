3ef481d11def35c7b6704a597f9ca5e04d0aec50
Willem Liu <willem_liu@hotmail.com>
2021-03-31 11:36:53 +0200

    removed logging from raw-loader

--- jest/transformers/raw-loader.js
+++ jest/transformers/raw-loader.js
@@ -1,6 +1,6 @@
 module.exports = {
     process: (content, filePath) => {
-        console.log('raw-loader', filePath);
+        // console.log('raw-loader', filePath);
         const val = JSON.stringify(content)
             .replace(/\u2028/g, '\\u2028')
             .replace(/\u2029/g, '\\u2029');
