553390abcaf7575cb91478b3dee1161929b925e1
Willem Liu <willem_liu@hotmail.com>
2021-09-24 14:45:21 +0200

    Quote can now receive className

--- src/components/quote/Quote.tsx
+++ src/components/quote/Quote.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './Quote.module.scss';
 
 interface Props {
+    className: string;
     blockquote: string;
     figcaption: string;
 }
 
 function Quote(props: Props) {
     return (
-        <figure className={styles.quote}>
+        <figure
+            className={`${styles.quote}${
+                props.className ? ` ${props.className}` : ``
+            }`}
+        >
             <blockquote>‘{props.blockquote}’</blockquote>
             <figcaption>• {props.figcaption}</figcaption>
         </figure>
