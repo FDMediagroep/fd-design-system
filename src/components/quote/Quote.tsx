57d85f77916351db378e60000e2bf5da1366fd61
Willem Liu <willem_liu@hotmail.com>
2021-09-24 14:46:29 +0200

    Quote can now receive optional className

--- src/components/quote/Quote.tsx
+++ src/components/quote/Quote.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Quote.module.scss';
 
 interface Props {
-    className: string;
+    className?: string;
     blockquote: string;
     figcaption: string;
 }
