2857e0d9942f8238655cea5f5d90585a3708fed6
Willem Liu <willem.liu@fdmediagroep.nl>
2021-12-02 15:29:19 +0100

    import react

--- src/components/footer/DarkModeToggle.tsx
+++ src/components/footer/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ThemeStore, { Theme } from '../../stores/ThemeStore';
 import styles from './Footer.module.scss';
 
