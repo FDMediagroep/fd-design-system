4c8688a948b2f18b0318b6df099f5b02e9d251e1
Alexander Eerenberg <alexander.eerenberg@fdmediagroep.nl>
2023-03-29 11:58:49 +0200

    FD-3616: Added tests, removed unnecessary style/component props, added always full height.

---
+++ __tests__/components/card/HorizontalCard3.test.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import {
+    HorizontalCard3,
+    getCssClassNames,
+} from '../../../src/components/card/HorizontalCard3';
+import { render } from '@testing-library/react';
+import { FdImgix } from '../../../src/components/utils/image/FdImgix';
+
+describe('HorizontalCard4', () => {
+    test('should render correctly', () => {
+        const { container, getByText } = render(
+            <HorizontalCard3
+                id="1"
+                url="https://example.com/1/This is the title"
+                title="This is the title"
+                isRead={false}
+                imageComponent={
+                    <FdImgix
+                        src="https://images.example.com/image.png"
+                        imageSizes={{
+                            widthS: '160px',
+                            widthM: '244px',
+                            widthL: '244px',
+                        }}
+                    />
+                }
+            />
+        );
+
+        expect(getByText('This is the title')).toBeTruthy();
+        expect(container?.querySelector('[href]')?.getAttribute('href')).toBe(
+            'https://example.com/1/This is the title'
+        );
+    });
+
+    test('should render image correctly', () => {
+        const { container } = render(
+            <HorizontalCard3
+                id="1"
+                url="https://example.com/1/This is the title"
+                title="This is the title"
+                isRead={false}
+                imageComponent={
+                    <FdImgix
+                        src="https://images.example.com/image.png"
+                        imageSizes={{
+                            widthS: '160px',
+                            widthM: '244px',
+                            widthL: '244px',
+                        }}
+                    />
+                }
+            />
+        );
+        expect(container?.querySelector('img')?.getAttribute('src')).toContain(
+            'https://images.example.com/image.png?auto=format'
+        );
+    });
+
+    test('should render is-read correctly', () => {
+        const { container } = render(
+            <HorizontalCard3
+                id="1"
+                url="https://example.com/1/This is the title"
+                title="This is the title"
+                isRead={true}
+                imageComponent={
+                    <FdImgix
+                        src="https://images.example.com/image.png"
+                        imageSizes={{
+                            widthS: '160px',
+                            widthM: '244px',
+                            widthL: '244px',
+                        }}
+                    />
+                }
+            />
+        );
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should have correct class name', () => {
+        const { container } = render(
+            <HorizontalCard3
+                id="1"
+                url="https://example.com/1/This is the title"
+                title="This is the title"
+                isRead={false}
+                imageComponent={
+                    <FdImgix
+                        src="https://images.example.com/image.png"
+                        imageSizes={{
+                            widthS: '160px',
+                            widthM: '244px',
+                            widthL: '244px',
+                        }}
+                    />
+                }
+            />
+        );
+
+        expect(
+            container.firstElementChild?.classList.contains(
+                getCssClassNames()[0]
+            )
+        ).toBeTruthy();
+    });
+
+    test('should render without image component', () => {
+        const { getByRole } = render(
+            <HorizontalCard3
+                id="1"
+                url="https://example.com/1/This is the title"
+                title="This is the title"
+                isRead={false}
+            />
+        );
+        const image = getByRole('img');
+        expect(image).toHaveProperty(
+            'src',
+            expect.stringContaining('/assets/images/fd-logo.svg')
+        );
+    });
+});
---
+++ pages/card/horizontal-3.tsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import { Explain } from '../../src/components/Explain';
+import {
+    HorizontalCard3,
+    getCssClassNames,
+} from '../../src/components/card/HorizontalCard3';
+import Head from 'next/head';
+import { globalCssClassNames } from '../../src/utils/globalCssClassNames';
+import styles from './cards.module.scss';
+import { FdImgix } from '../../src/components/utils/image/FdImgix';
+
+const horizontalCards3: any = [
+    {
+        id: '1343811',
+        url: 'https://fd.nl/podcasts/fd-achter-gesloten-deuren',
+        imageUrl:
+            'https://images.fd.nl/TH70Ic_7GBPn0vcNxQLf9Q9M4j8.jpg?fit=crop&crop=faces&auto=format,compress&q=45&w=300',
+        title: 'Achter Gesloten Deuren: wat is er gebeurd met Gerard Sanderink?',
+    },
+    {
+        id: '1343728',
+        url: 'https://fd.nl/podcasts/fd-dagkoers',
+        imageUrl:
+            'https://images.fd.nl/q5j62hGBLpQWOmFeNrlHp5bRT6E.jpg?fit=crop&crop=faces&auto=format,compress&q=45&w=300',
+        title: 'Dagkoers: in een kwartier op de hoogte van wat er speelt in de wereld van het FD',
+    },
+    {
+        id: '1343728',
+        url: 'https://fd.nl/podcasts/fd-de-week-voorbij',
+        imageUrl:
+            'https://images.fd.nl/Bcf9iiBg8tL9Qq1_gVHwDFGdFIg.jpg?rect=.0775%2c.0%2c.845%2c.9999999999999999&fit=crop&crop=faces&auto=format,compress&q=45&w=300',
+        title: 'De week voorbij: achtergronden bij het nieuws van het FD',
+        isRead: true,
+    },
+    {
+        id: '1334870',
+        url: 'https://fd.nl/podcasts/de-fd-gazellen-podcast',
+        title: 'FD Gazellen Podcast: verhalen over de snelst groeiende bedrijven van Nederland',
+    },
+];
+
+export const metaTitle = 'Horizontal Card 3';
+export const metaDescription = 'Horizontal Card 3, used on the homepage';
+
+function Page() {
+    return (
+        <>
+            <Head>
+                <title>
+                    {`${metaTitle} - Design System - Het Financieele Dagblad`}
+                </title>
+                <meta name="description" content={metaDescription} />
+                <meta property="og:title" content={metaTitle} />
+                <meta property="og:description" content={metaDescription} />
+                <meta property="twitter:title" content={metaTitle} />
+                <meta
+                    property="twitter:description"
+                    content={metaDescription}
+                />
+            </Head>
+
+            <Explain
+                previewClassName={`${styles.horizontalGrid} grid`}
+                anchor="horizontal3"
+                cssClassNames={[...getCssClassNames(), ...globalCssClassNames]}
+                legend="Horizontal Card 3"
+                reactComponentName="HorizontalCard3"
+                description={
+                    <>
+                        <p>Used for podcasts on the homepage</p>
+                        <ul>
+                            <li>default</li>
+                            <li>without a label</li>
+                            <li>visited</li>
+                            <li>no image</li>
+                        </ul>
+                    </>
+                }
+            >
+                <>
+                    <HorizontalCard3
+                        className="xs-12"
+                        {...horizontalCards3[0]}
+                        imageComponent={
+                            <FdImgix
+                                src={horizontalCards3[0].imageUrl}
+                                imageSizes={{
+                                    widthS: '80px',
+                                    widthM: '96px',
+                                    widthL: '120px',
+                                }}
+                            />
+                        }
+                    />
+                    <HorizontalCard3
+                        className="xs-12"
+                        {...horizontalCards3[1]}
+                        imageComponent={
+                            <FdImgix
+                                src={horizontalCards3[1].imageUrl}
+                                imageSizes={{
+                                    widthS: '80px',
+                                    widthM: '96px',
+                                    widthL: '120px',
+                                }}
+                            />
+                        }
+                    />
+                    <HorizontalCard3
+                        className="xs-12"
+                        {...horizontalCards3[2]}
+                        imageComponent={
+                            <FdImgix
+                                src={horizontalCards3[2].imageUrl}
+                                imageSizes={{
+                                    widthS: '80px',
+                                    widthM: '96px',
+                                    widthL: '120px',
+                                }}
+                            />
+                        }
+                    />
+                    <HorizontalCard3
+                        className="xs-12"
+                        {...horizontalCards3[3]}
+                    />
+                    <div
+                        className={`${styles['horizontal3-grid']} ${styles['autoMinHeight']} grid m__mt+3 xs-12 s-12 m-12 l-12`}
+                    >
+                        <div className={` xs-12 s-6 m-6 l-6`}>
+                            <HorizontalCard3
+                                {...horizontalCards3[0]}
+                                imageComponent={
+                                    <FdImgix
+                                        src={horizontalCards3[0].imageUrl}
+                                        imageSizes={{
+                                            widthS: '80px',
+                                            widthM: '96px',
+                                            widthL: '120px',
+                                        }}
+                                    />
+                                }
+                            />
+                        </div>
+                        <div className={` xs-12 s-6 m-6 l-6`}>
+                            <HorizontalCard3
+                                {...horizontalCards3[1]}
+                                imageComponent={
+                                    <FdImgix
+                                        src={horizontalCards3[1].imageUrl}
+                                        imageSizes={{
+                                            widthS: '80px',
+                                            widthM: '96px',
+                                            widthL: '120px',
+                                        }}
+                                    />
+                                }
+                            />
+                        </div>
+                        <div className={` xs-12 s-6 m-6 l-6`}>
+                            <HorizontalCard3
+                                {...horizontalCards3[2]}
+                                imageComponent={
+                                    <FdImgix
+                                        src={horizontalCards3[2].imageUrl}
+                                        imageSizes={{
+                                            widthS: '80px',
+                                            widthM: '96px',
+                                            widthL: '120px',
+                                        }}
+                                    />
+                                }
+                            />
+                        </div>
+                        <div className={` xs-12 s-6 m-6 l-6`}>
+                            <HorizontalCard3 {...horizontalCards3[3]} />
+                        </div>
+                    </div>
+                </>
+            </Explain>
+        </>
+    );
+}
+
+export default Page;
